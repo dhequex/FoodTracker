5986a685d4de5ca6db44c753f8d73e401df06c50
Jose Torres <app@conexionasiapacifico.com>
2021-03-04 01:30:44 +0900

    UI Integrating Maps view and Navigation

--- FoodTracker/FoodTrackerApp.swift
+++ FoodTracker/FoodTrackerApp.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 @main
 struct FoodTrackerApp: App {
+    
+    @StateObject var locations = Locations()
     var body: some Scene {
         WindowGroup {
-            NavigationView{
-                ContentView(location: Locations().primary)
+            TabView{
+                NavigationView{
+                ContentView(location: locations.primary)
         }
+            .tabItem {
+                Image(systemName: "graduationcap.fill")
+                Text("Learn more")
+            }
+                NavigationView{
+                    Worldview()
+                }
+                .tabItem { Image(systemName: "star.fill")
+                Text("Locations")
+                }
         }
+            .environmentObject(locations)
     }
+}
 }
---
+++ FoodTracker/Worldview.swift
@@ -0,0 +1,36 @@
+//
+//  Worldview.swift
+//  FoodTracker
+//
+//  Created by Jose M. Torres on 2021/03/04.
+//
+import MapKit
+import SwiftUI
+
+struct Worldview: View {
+    @EnvironmentObject  var locations: Locations
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude:36.6982, longitude: 137.8619),
+        span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    var body: some View {
+        Map(coordinateRegion: $region, annotationItems: locations.places){
+            location in MapAnnotation(coordinate: CLLocationCoordinate2D(latitude:location.latitude, longitude: location.longitude)){
+                NavigationLink(
+                    destination: ContentView(location: location)){
+                    Image(location.country)
+                        .resizable()
+                        .cornerRadius(10)
+                        .frame(width:80, height: 40)
+                        .shadow(radius:3)
+                }
+            }
+        }
+            .navigationTitle("Locations")
+    }
+}
+
+struct Worldview_Previews: PreviewProvider {
+    static var previews: some View {
+        Worldview()
+    }
+}
