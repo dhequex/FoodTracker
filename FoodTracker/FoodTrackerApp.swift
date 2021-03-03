c28deb60d5dc6c482823c8fe48712d77ccf6ef33
Jose Torres <app@conexionasiapacifico.com>
2021-03-03 20:56:30 +0900

    Initial commit

---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Jose M. Torres on 2021/03/03.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
