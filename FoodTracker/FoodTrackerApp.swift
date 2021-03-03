258838d2659e691e1ffa72a3c4759c9b04e29dd9
Jose Torres <app@conexionasiapacifico.com>
2021-03-03 23:47:38 +0900

    First landing screen UI

--- FoodTracker/FoodTrackerApp.swift
+++ FoodTracker/FoodTrackerApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct FoodTrackerApp: App {
     var body: some Scene {
         WindowGroup {
+            NavigationView{
             ContentView()
+        }
         }
     }
 }
